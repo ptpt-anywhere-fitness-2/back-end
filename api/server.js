c6c90783afd75ac54b8da6139711be31a9bafbc9
drewmrodriguez <drewmrodriguez@gmail.com>
2021-10-13 21:28:49 -0500

    intial set up

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
